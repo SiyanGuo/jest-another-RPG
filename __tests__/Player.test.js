036b4b70ab41b0ce4d3cc7563fc6bc90c0856525
Siyan Guo <siyang@Hey-There.local>
2021-08-20 10:39:29 -0400

    add methods to player obj

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,6 +2,29 @@ const Potion = require ('../lib/Potion');
 jest.mock('../lib/Potion');
 const Player = require ('../lib/Player');
 
+test("subtracts from player's health", ()=>{
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth-5);
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
+})
+
+test('check if player is alive or not', ()=>{
+    const player = new Player('Dave');
+    expect(player.isAlive()).toBeTruthy();
+    player.health= 0;
+    expect(player.isAlive()).toBeFalsy();
+})
+
+test("get player's health value", ()=>{
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()))
+})
+
 test("gets player's status as an object", ()=>{
     const player = new Player('Dave');
 
